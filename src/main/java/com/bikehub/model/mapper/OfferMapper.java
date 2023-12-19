aa5f25863649b5a1a4dd86e7ca2ee7339eb1c863
gladbazo <la_ter@abv.bg>
2023-12-19 19:53:18 +0200

    upload3

--- src/main/java/com/bikehub/model/mapper/OfferMapper.java
+++ src/main/java/com/bikehub/model/mapper/OfferMapper.java
@@ -1,9 +1,9 @@
-//package com.example.bookify.model.mapper;
+//package com.bikehub.model.mapper;
 //
-//import com.example.bookify.model.dto.offer.AddOfferDTO;
-//import com.example.bookify.model.dto.offer.OfferDetailsDTO;
-//import com.example.bookify.model.entity.Offer;
-//import com.example.bookify.model.view.ReservationsViewModel;
+//import com.bikehub.model.dto.offer.AddOfferDTO;
+//import com.bikehub.model.dto.offer.OfferDetailsDTO;
+//import com.bikehub.model.entity.Offer;
+//import com.bikehuby.model.view.ReservationsViewModel;
 //import org.mapstruct.Mapper;
 //
 //@Mapper(componentModel = "spring")
